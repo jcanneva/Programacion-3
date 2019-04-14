90932151c94d2fc251b26f3fe53569d12025c8ff
jcanneva <jcanneva@alumnos.exa.unicen.edu.ar>
2019-04-14 01:32:00 -0300

    dfs

--- Practico3/Grafo.java
+++ Practico3/Grafo.java
@@ -10,7 +10,7 @@ public class Grafo {
 	
 	final static String VISITADO="amarillo";
 	final static String NO_VISITADO="blanco";
-	final static String YA_VISITADO="negro";
+	final static String TERMINADO="negro";
 	
 	public Grafo() {
 		this.arcos=new ArrayList<Arco>();
@@ -20,6 +20,29 @@ public class Grafo {
 	public void addVertice(Vertice v) {
 		if (!this.vertices.contains(v))
 			this.vertices.add(v);
+	}
+	
+	public void DFS() {
+		for (Vertice v : this.vertices) {
+			v.setEstado(NO_VISITADO);
+		} 
+		for (Vertice v : this.vertices) {
+			if(v.getEstado()==NO_VISITADO) 
+				DFS_visitar(v);
+		} 
+	}
+
+	private void DFS_visitar(Vertice v) {
+		v.setEstado(VISITADO);
+		ArrayList<Vertice> ady = v.getAdyacentes();
+		for (Vertice tmp : ady) {
+			if(tmp.getEstado()==NO_VISITADO) 
+				DFS_visitar(tmp);
+			//si quiero saber si hay ciclos
+			//else if (tmp.getEstado()==VISITADO)
+			//		return true;
+		}
+		v.setEstado(TERMINADO);
 	}
 	
 }
